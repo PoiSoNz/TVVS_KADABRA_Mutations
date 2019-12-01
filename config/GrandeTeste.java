1c1e77a70ec0615a2e7dba40969b3d20023aede4
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2019-12-01 23:32:01 +0000

    Bitwise Operators working (needs verification)

---
+++ config/GrandeTeste.java
@@ -0,0 +1,25 @@
+
+
+public class GrandeTeste {
+    private static int K = 0;
+
+    /**
+     * Edge detection operation that uses three convolution steps:
+     *   1. Gaussian
+     *   2. Vertical
+     *   3. Horizontal
+     * A binary image is outputed based on a threshold.
+     */
+    public static int GrandeTeste(int[][] inputImage) {
+        GrandeTeste.K = 3;
+        int c = (GrandeTeste.K);
+        int d = GrandeTeste.K;
+        int e = 5;
+        GrandeTeste.K += 1;
+        GrandeTeste.K = (GrandeTeste.K);
+        GrandeTeste.K = (GrandeTeste.K) | 4;
+        Boolean b = (GrandeTeste.K) < 4;
+        return GrandeTeste.K;
+    }
+}
+
--- java/GrandeTeste.java
+++ java/GrandeTeste.java
@@ -10,8 +10,15 @@ public class GrandeTeste {
 	 * A binary image is outputed based on a threshold.
 	 */
 	public static int GrandeTeste(int[][] inputImage) {
+		K = 3;
+		int c = ~K;
+		int d = K;
+		int e = ~5;
 		K += 1;
 		K = ~K;
+		K = K | 4;
+		
+		Boolean b = K < 4;
 		
 		return K;
 	}
