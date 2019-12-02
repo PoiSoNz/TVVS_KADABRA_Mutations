bfee099eb48a6281da594a60e300349a06c92d9b
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2019-12-02 18:07:41 +0000

    bitwise operator mutator finalizated and tested

--- config/GrandeTeste.java
+++ config/GrandeTeste.java
@@ -12,11 +12,11 @@ public class GrandeTeste {
      */
     public static int GrandeTeste(int[][] inputImage) {
         GrandeTeste.K = 3;
-        int c = (GrandeTeste.K);
+        int c = ~(GrandeTeste.K);
         int d = GrandeTeste.K;
-        int e = 5;
+        int e = ~5;
         GrandeTeste.K += 1;
-        GrandeTeste.K = (GrandeTeste.K);
+        GrandeTeste.K = ~(GrandeTeste.K);
         GrandeTeste.K = (GrandeTeste.K) | 4;
         Boolean b = (GrandeTeste.K) < 4;
         return GrandeTeste.K;
