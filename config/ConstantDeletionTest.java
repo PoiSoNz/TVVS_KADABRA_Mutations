8c897fcc1616034caebf86558e7b1d0ad74d659a
Nelson Almeida <up201505394@fe.up.pt>
2019-12-16 23:25:22 +0000

    fix constant deletion mutator on non constants; add print when no mutation;

--- config/ConstantDeletionTest.java
+++ config/ConstantDeletionTest.java
@@ -9,17 +9,23 @@ public class ConstantDeletionTest {
 
     final int d = 244;
 
+    int h = 244;
+
     public ConstantDeletionTest() {
         int e = a;
         e = b;
         e = 3;
         int f = (a) * 3;
         f = (a) + (a);
-        f = (((b) + (c)) - (d)) + 3;
+        f = ((((b) + (c)) - (d)) + 3) + e;
         if ((c) < (((d) + ((3 * (a)) / ((c) * (c)))) - 2)) {
             f = ((b) / (a)) + (d);
             boolean i = (((c) + (d)) + 2) > (((a) + ((3 * (b)) / (d))) - 2);
+            final int g = 3;
+            e = (g * (c)) + (h);
         }
+        int g = 1;
+        e = (h) + (g * 2);
     }
 }
 
