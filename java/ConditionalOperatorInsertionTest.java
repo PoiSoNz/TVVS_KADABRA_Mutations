bf77f15eea74d573c721b6e3962a7e6803b9f2a3
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2019-12-16 07:33:04 +0000

    ConditionalOperatorInsertionMutator done

---
+++ java/ConditionalOperatorInsertionTest.java
@@ -0,0 +1,24 @@
+public class ConditionalOperatorInsertionTest{
+	
+	public int k = 0;
+	
+	public ConditionalOperatorInsertionTest() {
+		
+		Boolean a = true;
+		
+		if(a)
+			k += 1;
+		
+		if(k > 0)
+			k += 10;
+		
+		if(!a)
+			k = 0;
+		
+		if(a && k > 0)
+			k += 100;
+		
+		if(k == 0 || ( a && k > 0))
+			k += 1000;
+	}
+}
