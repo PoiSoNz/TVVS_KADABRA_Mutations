27d7c73aff9f731b9fcff063b9173e9401d1e2e8
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2019-12-16 08:27:12 +0000

    added support for ifs with ternary operator

--- config/ConditionalOperatorDeletionTest.java
+++ config/ConditionalOperatorDeletionTest.java
@@ -14,6 +14,11 @@ public class ConditionalOperatorDeletionTest {
         if (!(i > 3)) {
             i = 5;
         }
+        String s = (i > 0) ? "q" : "w";
+        String s2 = (b && (i > 0)) ? "e" : "r";
+        String s3 = ((i == 0) || (b && (i > 0))) ? "a" : "s";
+        String s4 = (!((i == 0) || (b && (i > 0)))) ? "d" : "f";
+        String s5 = (!b) ? "d" : "f";
     }
 }
 
--- config/ConditionalOperatorInsertionTest.java
+++ config/ConditionalOperatorInsertionTest.java
@@ -20,6 +20,11 @@ public class ConditionalOperatorInsertionTest {
         if (((k) == 0) || (a && ((k) > 0)))
             k += 1000;
 
+        String s = ((k) > 0) ? "q" : "w";
+        String s2 = (a && ((k) > 0)) ? "e" : "r";
+        String s3 = (((k) == 0) || (a && ((k) > 0))) ? "a" : "s";
+        String s4 = (!(((k) == 0) || (a && ((k) > 0)))) ? "d" : "f";
+        String s5 = (!a) ? "d" : "f";
     }
 }
 
